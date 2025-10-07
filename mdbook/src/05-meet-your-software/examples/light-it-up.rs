9c06c559f5450e571248cad0479211caba6efa15
Igor Semyonov <igor@semyonov.xyz>
2025-10-06 23:01:59 -0400

    modified light-it-up to blink using very crude mechanism of looping nop

--- mdbook/src/05-meet-your-software/examples/light-it-up.rs
+++ mdbook/src/05-meet-your-software/examples/light-it-up.rs
@@ -2,17 +2,27 @@
 #![no_main]
 #![no_std]
 
+use cortex_m::asm;
 use cortex_m_rt::entry;
 use embedded_hal::digital::OutputPin;
 use microbit::board::Board;
 use panic_halt as _;
 
+const WAIT_CYCLES: u32 = 250_000;
+
 #[entry]
 fn main() -> ! {
     let mut board = Board::take().unwrap();
 
-    board.display_pins.col1.set_low().unwrap();
-    board.display_pins.row1.set_high().unwrap();
-
-    loop {}
+    loop {
+        board.display_pins.col1.set_low().unwrap();
+        board.display_pins.row1.set_high().unwrap();
+        for _ in 0..WAIT_CYCLES {
+            asm::nop();
+        }
+        board.display_pins.col1.set_high().unwrap();
+        for _ in 0..WAIT_CYCLES {
+            asm::nop();
+        }
+    }
 }
